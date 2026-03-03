673b63b66f0e1679e9890aa95d094aa95c3f922a
FELIPE AUGUSTO DA SILVA <felipe.silva522@senaisp.edu.br>
2026-03-03 20:22:01 -0300

    Correct variable names and add metal total calculation

--- App.js
+++ App.js
@@ -1,10 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [ocorrencia, setOcorrencia] = useState("");
+  const [pin, setPin] = useState("");
+  const [pesoCromo, setPesoCromo] = useState("");
+  const [pesoNiquel, setPesoNiquel] = useState("");
+  const [totalMetais, setTotalMetais] = useState(0);
+
+  function calculaMetais() {
+    const Cromo = parseFloat(pesoCromo.replace(",", ".")) || 0;
+    const Niquel = parseFloat(pesoNiquel.replace(",", ".")) || 0;
+
+    const total = Cromo + Niquel;
+    setTotalMetais(total);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Tech Alloy - Controle de Produção</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Digite a ocorrência aqui"
+        value={ocorrencia}
+        onChangeText={setOcorrencia}
+      />
+
+      <Text>PIN de segurança</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu pin"
+        value={pin}
+        onChangeText={setPin}
+        secureTextEntry={true}
+      />
+
+      <Button title="ENVIAR OCORRÊNCIA" color="#ff4444" />
+
+      <Text style={styles.subtitulo}>Relatório de Ocorrências</Text>
+
+      <Text>Carga de Cromo (Kg)</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="0,00"
+        value={pesoCromo}
+        onChangeText={setPesoCromo}
+        keyboardType="numeric"
+      />
+
+      <Text>Carga de Níquel (Kg)</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="0,00"
+        value={pesoNiquel}
+        onChangeText={setPesoNiquel}
+        keyboardType="numeric"
+      />
+
+      <Button
+        title="CALCULAR TOTAL DE METAIS"
+        color="#4444ff"
+        onPress={calculaMetais}
+      />
+
+      <Text style={styles.resultado}>
+        Peso Total de Carga: {totalMetais.toFixed(2)} Kg
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +77,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
